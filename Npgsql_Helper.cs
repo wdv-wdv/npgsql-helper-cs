01d4dd9d43fb0b232237bb29351db33fe53907ee
WDV <Git@wdv.name>
2015-10-04 01:19:33 +0100

    Initial upload

---
+++ Npgsql_Helper.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Npgsql;
+using System.Data;
+
+namespace Npgsql_Helper_
+{
+    public class Npgsql_Helper
+    {
+        public Npgsql_Helper(String connectionString)
+        {
+            _conn = connectionString;
+        }
+
+        //####  private methods ###
+        private string _conn = null;
+
+        private NpgsqlCommand GetCommand(string query, NpgsqlParameter[] npgsqlParameters, CommandType commandType)
+        {
+            NpgsqlConnection conn = new NpgsqlConnection(_conn);
+            conn.UseSslStream = false;
+            conn.Open();
+
+            query = query.ToLower();
+
+            NpgsqlCommand command = new NpgsqlCommand(query, conn);
+            command.CommandType = commandType;
+    
+            if (npgsqlParameters is NpgsqlParameter[])
+            {
+                command.Parameters.AddRange(npgsqlParameters);
+            }
+
+            return command;
+        }
+
+        //#### public methods ####
+
+        public  long ExecuteNonQuery(string query, NpgsqlParameter[] npgsqlParameters)
+        {
+            return ExecuteNonQuery(CommandType.StoredProcedure, query, npgsqlParameters);
+        }
+
+        public  long ExecuteNonQuery(CommandType commandType, string query, NpgsqlParameter[] npgsqlParameters)
+        {
+
+            using (NpgsqlCommand command = GetCommand(query, npgsqlParameters, commandType))
+            {
+                Int32 rowsaffected;
+
+                try
+                {
+                    rowsaffected = command.ExecuteNonQuery();
+                    return rowsaffected;
+                }
+                catch (Exception Ex)
+                {
+                    throw Ex;
+                }
+
+                finally
+                {
+                    command.Connection.Close();
+                }
+            }
+
+        }
+
+        public  long ExecuteNonQuery(CommandType commandType, string query)
+        {
+            return ExecuteNonQuery(commandType, query, null);
+        }
+
+        public  object ExecuteScalar(string query, NpgsqlParameter[] npgsqlParameters)
+        {
+            return ExecuteScalar(CommandType.StoredProcedure, query, npgsqlParameters);
+        }
+
+        public  object ExecuteScalar(CommandType commandType, string query, NpgsqlParameter[] npgsqlParameters)
+        {
+            using (NpgsqlCommand command = GetCommand(query, npgsqlParameters, commandType))
+            {
+                object result;
+
+                try
+                {
+                    result = command.ExecuteScalar();
+                    return result;
+                }
+                catch (Exception Ex)
+                {
+                    throw Ex;
+                }
+                finally
+                {
+                    command.Connection.Close();
+                }
+            }
+        }
+
+        public  object ExecuteScalar(CommandType commandType, string query)
+        {
+            return ExecuteScalar(commandType, query, null);
+        }
+
+        public  DataTable[] ExecuteDataset(string query, NpgsqlParameter[] npgsqlParameters)
+        {
+            return ExecuteDataset(CommandType.StoredProcedure, query, npgsqlParameters);
+
+        }
+
+        public  DataTable[] ExecuteDataset(CommandType commandType, string query, NpgsqlParameter[] npgsqlParameters)
+        {
+            using (NpgsqlCommand command = GetCommand(query, npgsqlParameters, commandType))
+            {
+                try
+                {
+                    DataSet myDS = new DataSet();
+
+                    NpgsqlTransaction t = command.Connection.BeginTransaction();
+
+                    NpgsqlDataAdapter da = new NpgsqlDataAdapter(command);
+                    da.Fill(myDS);
+
+                    t.Commit();
+
+                    DataTable[] tables = new DataTable[myDS.Tables.Count];
+
+                    myDS.Tables.CopyTo(tables, 0);
+
+                    return tables;
+
+                }
+                catch (Exception Ex)
+                {
+                    throw Ex;
+                }
+
+
+                finally
+                {
+                    command.Connection.Close();
+                }
+            }
+        }
+
+        public  DataTable[] ExecuteDataset(CommandType commandType, string query)
+        {
+            return ExecuteDataset(commandType, query, null);
+        }
+    }
+
+}
---
+++ Test_examples/Test_examples/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Npgsql_Helper_;
+using System.Data;
+using Npgsql;
+
+namespace Test_examples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //#### Initialise #####
+            string conn = @"Server=dbserver;Database=dellstore2;User Id=dellstore;Password=pa$$word;";//sslmode=disable;ssl=false;Protocol=3;Pooling=true;MaxPoolSize=30";
+            Npgsql_Helper dellstoreDB = new Npgsql_Helper(conn);
+
+            // #### a simple query ####
+            {
+                string sql = "select * from products where special = :special";
+
+                NpgsqlParameter[] p = new NpgsqlParameter[1];
+                p[0] = new NpgsqlParameter("special", DbType.Int16);
+                p[0].Value = 1;
+
+                DataTable result = dellstoreDB.ExecuteDataset(CommandType.Text, sql, p)[0];
+                foreach (DataRow row in result.Rows)
+                {
+                    //Do something ....
+                }
+            }
+
+            // #### a scalar query ####
+            {
+                string sql = "select count(*) from products where special = :special";
+
+                NpgsqlParameter[] p = new NpgsqlParameter[1];
+                p[0] = new NpgsqlParameter(":special", DbType.Int16);
+                p[0].Value = 1;
+
+                Object result = dellstoreDB.ExecuteScalar(CommandType.Text, sql, p);
+            }
+
+            // #### a simple update query ####
+            {
+                string sql = "update products  set special = 1  where special = :special";
+
+                NpgsqlParameter[] p = new NpgsqlParameter[1];
+                p[0] = new NpgsqlParameter("special", DbType.Int16);
+                p[0].Value = 1;
+
+                long l = dellstoreDB.ExecuteNonQuery(CommandType.Text, sql, p);
+                
+                
+            }
+        }
+    }
+}
