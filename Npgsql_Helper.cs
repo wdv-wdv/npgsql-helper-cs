8ba26fd0712a781a49c98ef8312cdb6452b9fc26
WDV <github@wdv.name>
2015-11-25 10:15:07 +0000

    Update Npgsql_Helper.cs

--- Npgsql_Helper.cs
+++ Npgsql_Helper.cs
@@ -1,4 +1,10 @@
-﻿using System;
+/* Npgsql helper library for C# Copyright (C) 2015 Kionier
+For guides on how to use this library, please go to http://kionier.com/npgsql-helper/.
+This software is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License (GPL) or GNU Lesser General Public License (LGPL) as published by the Free Software Foundation, either version 2 or later.
+This software is distributed WITHOUT ANY WARRANTY; see the GNU General Public License for more details. http://www.gnu.org/licenses/.
+*/
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
